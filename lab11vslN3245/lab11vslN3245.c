b948a80e667d20c22329e64126b9803d185d00c0
Vladislav <amurchik2003@inbox.ru>
2024-04-09 01:34:34 +0300

    addsd jokes. its really important

--- lab11vslN3245/lab11vslN3245.c
+++ lab11vslN3245/lab11vslN3245.c
@@ -3,9 +3,29 @@
 #include <string.h>
 #include <ftw.h>
 #include <getopt.h>
+#include <time.h>
 
 const char* searchString; // Глобальная переменная для хранения строки для поиска
 int found = 0;
+
+// Список шуток
+const char* jokes[] = {
+    "Штирлиц на даче открыл дверь. Включился свет. Штирлиц закрыл дверь. Свет выключился. Холодильник, догадался он.",
+    "Из окна дуло. Штирлиц подошел к окну. Дуло исчезло.",
+    "Штирлиц топил печку. Через час печка утонула.",
+    "Штирлиц мог спать сутками. Но утки с ним спать не хотели.",
+    "Встретив гестаповцев, Штирлиц выхватил шашку и закричал: “Порублю!”. Гестаповцы скинулись по рублю и убежали.",
+    "Штирлиц вошёл в комнату, из окна дуло. Штирлиц закрыл окно, дуло исчезло.",
+    "Штирлиц попал в глубокую яму и чудом из нее вылез. «Чудес не бывает», — подумал Штирлиц и на всякий случай залез обратно.",
+    "Штирлицу за шиворот упала гусеница. «Неподалеку взорвался танк», — догадался Штирлиц."
+};
+const int jokesCount = sizeof(jokes) / sizeof(jokes[0]);
+
+void printJoke() {
+    srand(time(NULL)); // Инициализация генератора случайных чисел
+    int jokeIndex = rand() % jokesCount; // Получение случайного индекса
+    printf("%s\n", jokes[jokeIndex]); // Вывод шутки
+}
 
 void debugPrint(const char *message) {
     if (getenv("LAB11DEBUG") != NULL) {
@@ -56,10 +76,11 @@ int processEntry(const char *fpath, const struct stat *sb, int typeflag) {
 
 void printHelp() {
     printf("Использование:\n");
-    printf("lab11vslN3245 [опции] <директория> \"<строка для поиска>\"\n");
+    printf("lab11vslN3245 [опции] <директория> \"строка для поиска\"\n");
     printf("Опции:\n");
     printf("-v, --version    Вывод версии программы\n");
     printf("-h, --help       Вывод этой справки\n");
+    printf("-j, --joke       Вывод случайной шутки\n");
 }
 
 void printVersion() {
@@ -71,16 +92,20 @@ int main(int argc, char *argv[]) {
     static struct option long_options[] = {
         {"help", no_argument, 0, 'h'},
         {"version", no_argument, 0, 'v'},
+        {"joke", no_argument, 0, 'j'}, // Добавление новой опции
         {0, 0, 0, 0}
     };
 
-    while ((opt = getopt_long(argc, argv, "hv", long_options, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "hvj", long_options, NULL)) != -1) {
         switch (opt) {
             case 'h':
                 printHelp();
                 return EXIT_SUCCESS;
             case 'v':
                 printVersion();
+                return EXIT_SUCCESS;
+            case 'j':
+                printJoke(); // Вызов функции вывода шутки
                 return EXIT_SUCCESS;
             default:
                 printHelp();
